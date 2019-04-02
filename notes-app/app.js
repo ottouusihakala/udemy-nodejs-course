16f63b5aac326ce3ae1cfa5d620dbf0e7c417a09
Otto Uusihakala <otto.uusihakala@gmail.com>
2019-04-02 10:30:43 +0300

    completed lecture 9 challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,4 @@
 const fs = require('fs')
-fs.writeFileSync('notes.txt', 'Another test of fs')
+//fs.writeFileSync('notes.txt', 'Another test of fs')
+
+fs.appendFileSync('notes.txt', 'Appended text')
